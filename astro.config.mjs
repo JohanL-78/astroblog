8c6a62776c929e806ecbe776abbefabe40f91875
Johan Lorck <kfj.lorck@orange.fr>
2026-01-10 20:01:53 +0100

    Configure Cloudflare adapter for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,12 @@
 import { defineConfig } from 'astro/config';
 import sanity from '@sanity/astro';
 import react from '@astrojs/react';
-import node from '@astrojs/node';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  }),
+  adapter: cloudflare(),
   devToolbar: {
     enabled: false
   },
