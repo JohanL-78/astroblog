68c131ad5ca00071ef7b42eacefba1a65569d1f5
Johan Lorck <kfj.lorck@orange.fr>
2026-05-04 17:48:21 +0200

    cloud adapt

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,11 @@
 import { defineConfig } from 'astro/config';
 import sanity from '@sanity/astro';
 import react from '@astrojs/react';
-import cloudflare from '@astrojs/cloudflare';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static', // 🌱 Static Site Generation for better performance and eco-friendliness
-  adapter: cloudflare(), // Keep adapter for Cloudflare Pages deployment
   devToolbar: {
     enabled: false
   },
